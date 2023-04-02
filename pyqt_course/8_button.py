1fe4ca5112b761c4f656158ed51b375da6ab056e
Alex <al.an.gukov@gmail.com>
2023-04-02 22:23:29 +0200

    addition to button actions message boxes: warning, infromation, about types

--- pyqt_course/8_button.py
+++ pyqt_course/8_button.py
@@ -1,5 +1,5 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMenu
-from PyQt6.QtGui import QIcon, QFont
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMenu, QMessageBox
+from PyQt6.QtGui import QIcon, QFont, QAction
 from PyQt6.QtCore import QSize
 import sys
 
@@ -25,10 +25,32 @@ class Window(QWidget):
         menu = QMenu()
         menu.setFont(QFont('Times New Roman', 14, QFont.Weight.ExtraLight))
         menu.setStyleSheet('background-color:lightblue')
-        menu.addAction('Copy')
-        menu.addAction('Cut')
-        menu.addAction('Paste')
+
+        # add the "Copy" action to the menu
+        copy_action = QAction('Copy', self)
+        copy_action.triggered.connect(self.handle_copy_action)
+        menu.addAction(copy_action)
+
+        # add the "Cut" action to the menu
+        cut_action = QAction('Cut', self)
+        cut_action.triggered.connect(self.handle_cut_action)
+        menu.addAction(cut_action)
+
+        # add the "Paste" action to the menu
+        paste_action = QAction('Paste', self)
+        paste_action.triggered.connect(self.handle_paste_action)
+        menu.addAction(paste_action)
+
         btn.setMenu(menu)
+
+    def handle_copy_action(self):
+        QMessageBox.warning(self, 'Copy', 'You selected Copy')
+
+    def handle_cut_action(self):
+        QMessageBox.information(self, 'Cut', 'You selected Cut')
+
+    def handle_paste_action(self):
+        QMessageBox.about(self, 'Paste', 'You selected Paste')
 
 
 app = QApplication(sys.argv)
